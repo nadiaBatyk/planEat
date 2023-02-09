fa813e85770e236243bea44b138359ad0bc60827
nadub10 <nadia.batyk@gmail.com>
2023-02-09 12:14:10 -0300

    basic folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 
 const app = express()
+// MIDDLEWARES
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const PORT = 3000
-app.get('/hola', (_req, res) => {
-  console.log('alguien me saluda')
-  console.log('holis')
-  console.log('soy pinkyyy aaa')
-  // const arr = []
-  res.send('como estas??')
+// ROUTES
+app.get('/meals', (_req, res) => {
+  console.log('piden comidas')
+
+  res.send('Estas son tus comidas')
 })
+
+const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
-  console.log(`conectado con exito al puerto ${PORT}`)
+  console.log(`Succesfully connected to port ${PORT}`)
 })
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+app.on('error', (err) => console.log(`Error on server: ${err}`))
---
+++ src/routes/mealRoutes.ts
@@ -0,0 +1,4 @@
+import { Router } from 'express'
+
+const mealRouter = Router()
+mealRouter.route('/').get()
