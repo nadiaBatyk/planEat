27a761b8fbbf75857cf08268c5beee269a550edc
nadub10 <nadia.batyk@gmail.com>
2023-02-16 12:38:03 -0300

    pre change linter

--- src/controllers/mealController.ts
+++ src/controllers/mealController.ts
@@ -1,71 +1,71 @@
-import { Request, Response } from "express";
-import { Meal1 } from "../db/models/Meal";
-import { MealType1 } from "../db/models/MealType";
-import { Ingredient1 } from "../db/models/Ingredient";
-import { MealIngredient1 } from "../db/models/MealIngredient";
+import { Request, Response } from 'express'
+import { Meal } from '../db/models/Meal'
+import { MealType } from '../db/models/MealType'
+import { Ingredient } from '../db/models/Ingredient'
+import { MealIngredient } from '../db/models/MealIngredient'
 
 const getMeals = async (_req: Request, res: Response): Promise<void> => {
-  const f = await Meal1.findAll({
-    include: [MealType1, Ingredient1],
-    order: [["id", "ASC"]],
-  });
+  const f = await Meal.findAll({
+    include: [MealType, Ingredient],
+    order: [['id', 'ASC']]
+  })
   /*   const g = await m.getIngredients()
   console.log(i.dataValues)
   console.log(g) */
-  res.send(f);
-};
+  res.send(f)
+}
 const getMealTypes = async (_req: Request, res: Response): Promise<void> => {
-  const f = await MealType1.findAll({ include: [Meal1] });
-  res.send(f);
-};
+  const f = await MealType.findAll({ include: [Meal] })
+  res.send(f)
+}
 const getIngredients = async (_req: Request, res: Response): Promise<void> => {
-  const mt = await Ingredient1.findAll();
-  res.send(mt);
-};
+  const mt = await Ingredient.findAll()
+  res.send(mt)
+}
 const createIngredient = async (
   _req: Request,
   res: Response
 ): Promise<void> => {
-  const { name, unit } = _req.body;
-  const m = await Ingredient1.create({ name, unit });
-  console.log(m);
-  res.send(m.dataValues);
-};
+  const { name, unit } = _req.body
+  const m = await Ingredient.create({ name, unit })
+  console.log(m)
+  res.send(m.dataValues)
+}
 const createType = async (_req: Request, res: Response): Promise<void> => {
-  const { name } = _req.body;
-  const m = await MealType1.create({ name });
-  console.log(m);
-  res.send(m.dataValues);
-};
+  const { name } = _req.body
+  const m = await MealType.create({ name })
+  console.log(m)
+  res.send(m.dataValues)
+}
 const createMeal = async (_req: Request, res: Response): Promise<Response> => {
-  const { name, mealTypeId, ingredient } = _req.body;
-  const m = await Meal1.create({
+  const { name, mealTypeId, ingredient } = _req.body
+  const m = await Meal.create({
     name,
-    mealTypeId,
-  });
-  console.log(m.dataValues);
-  const i = await Ingredient1.create({
+    mealTypeId
+  })
+  console.log(m.dataValues)
+  const i = await Ingredient.create({
     name: ingredient.name,
-    unit: ingredient.unit,
-  });
-  await m.$add("ingredient", i, {
-    through: { model: MealIngredient1, quantity: ingredient.quantity },
-  });
+    unit: ingredient.unit
+  })
+  await m.$add('ingredient', i, {
+    through: { model: MealIngredient, quantity: ingredient.quantity }
+  })
 
-  return res.send(m.dataValues);
-};
+  return res.send(m.dataValues)
+}
 const editMeal = async (_req: Request, res: Response): Promise<void> => {
-  const { ingredients } = _req.body;
-  const { id } = _req.params;
-  const m = await Meal1.findByPk(id);
+  const { ingredients } = _req.body
+  const { id } = _req.params
+  const m = await Meal.findByPk(id)
   ingredients.forEach(async (i: any) => {
-    await m?.$add("ingredients", i.id, {
-      through: { model: MealIngredient1, quantity: i.quantity },
-    });
-  });
-  console.log(m);
-  res.send(m?.dataValues);
-};
+    await m?.$add('ingredients', i.id, {
+      through: { model: MealIngredient, quantity: i.quantity }
+    })
+  })
+  console.log(m)
+  res.send(m?.dataValues)
+}
 
 export default {
   getMeals,
@@ -74,5 +74,5 @@ export default {
   createType,
   getIngredients,
   createIngredient,
-  editMeal,
-};
+  editMeal
+}
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,11 @@
-// import path from 'path'
-import { Sequelize } from "sequelize-typescript";
-import { Meal1 } from "./models/Meal";
-import { MealType1 } from "./models/MealType";
-import { Ingredient1 } from "./models/Ingredient";
-import { MealIngredient1 } from "./models/MealIngredient";
+import path from 'path'
+import { Sequelize } from 'sequelize-typescript'
 
 export const sequelize = new Sequelize({
   database: process.env.PGDATABASE as string,
-  dialect: "postgres",
+  dialect: 'postgres',
   username: process.env.PGUSER as string,
   password: process.env.PGPASSWORD,
   storage: process.env.PGHOST,
-  models: [Meal1, MealType1, Ingredient1, MealIngredient1],
-});
+  models: [path.join(__dirname, '/models')]
+})
