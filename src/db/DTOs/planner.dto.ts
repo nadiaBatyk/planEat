67fdbcb40382165d232be5bdf059cbbd2a99e2b5
nadiaBatyk <nadia.batyk@gmail.com>
2023-03-09 12:01:10 -0300

    planner and plannerMeal dto + docs

---
+++ src/db/DTOs/planner.dto.ts
@@ -0,0 +1,74 @@
+import { MealDTOResponse } from './meal.dto'
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     PlannerDTOResponse:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:
+ *           type: string
+ *           example: Meal prep for the week
+ *         startDate:
+ *           type: integer
+ *           format: date
+ *         finishDate:
+ *           type: integer
+ *           format: date
+ *         active:
+ *           type: boolean
+ *         meals:
+ *           type: array
+ *           items:
+ *              $ref: "#/components/schemas/MealDTOResponse"
+ */
+export interface PlannerDTOResponse {
+  id?: number
+  name: string
+  startDate: Date
+  finishDate: Date
+  active: boolean
+  meals?: MealDTOResponse[]
+}
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     PlannerDTORequest:
+ *       type: object
+ *       required:
+ *         - name
+ *         - startDate
+ *         - finishDate
+ *         - active
+ *       properties:
+ *         name:
+ *           type: string
+ *           example: Meal prep for the week
+ *         startDate:
+ *           type: integer
+ *           format: date
+ *         finishDate:
+ *           type: integer
+ *           format: date
+ *         active:
+ *           type: boolean
+ */
+export type PlannerDTORequest = Omit<PlannerDTOResponse, 'meals' | 'id'>
+
+/**
+ * @openapi
+ * components:
+ *   parameters:
+ *     plannerId:
+ *         name: plannerId
+ *         in: path
+ *         description: ID of planner to return
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           format: int64
+ */
---
+++ src/db/DTOs/plannerMeal.dto.ts
@@ -0,0 +1,45 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     PlannerMealDTOResponse:
+ *       type: object
+ *       properties:
+ *         plannerId:
+ *           type: integer
+ *         mealId:
+ *           type: integer
+ *         mealTypeId:
+ *           type: integer
+ *         mealDate:
+ *           type: integer
+ *           format: date
+ */
+export interface PlannerMealDTOResponse {
+  plannerId: number
+  mealId: number
+  mealTypeId: number
+  mealDate: Date
+}
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     PlannerMealDTOResponse:
+ *       type: object
+ *       required:
+ *         - mealId
+ *         - mealTypeId
+ *         - mealDate
+ *       properties:
+ *         plannerId:
+ *           type: integer
+ *         mealId:
+ *           type: integer
+ *         mealTypeId:
+ *           type: integer
+ *         mealDate:
+ *           type: integer
+ *           format: date
+ */
+export type PlannerMealDTORequest = Omit<PlannerMealDTOResponse, 'plannerId'>
