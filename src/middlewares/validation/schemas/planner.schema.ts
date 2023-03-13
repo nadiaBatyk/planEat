39de451a4c7f5454e03158287765c7042f393143
nadiaBatyk <nadia.batyk@gmail.com>
2023-03-12 23:41:04 -0300

    zod planner date validation wip

--- src/middlewares/validation/schemas/planner.schema.ts
+++ src/middlewares/validation/schemas/planner.schema.ts
@@ -10,18 +10,34 @@ export const PlannerSchema = z.object({
     .min(1, 'Planner name cannot be empty')
     .max(100, 'Planner name max length is 100 characters'),
   startDate: z
-    .date({
+    .string({
       required_error: 'startDate is required',
-      invalid_type_error: 'startDate must be a date',
+      invalid_type_error:
+        'startDate must be a iso datetime string ex. "2023-01-01T00:00:00+02:00"',
     })
-    .min(new Date(), { message: 'startDate must be a future date' }),
-  finishDate: z.date({
-    required_error: 'finishDate is required',
-    invalid_type_error: 'finishDate must be a date',
-  }),
+    .datetime({ offset: true })
+    .refine(startDate => startDate >= new Date().toISOString(), {
+      message: `start date can't be a past date`,
+    }),
+  finishDate: z
+    .string({
+      required_error: 'finishDate is required',
+      invalid_type_error:
+        'finishDate must be a iso datetime string ex. "2023-01-01T00:00:00+02:00"',
+    })
+    .datetime({ offset: true })
+    .refine(finishDate => finishDate >= new Date().toISOString(), {
+      message: `finish Date can't be a past date`,
+    }),
   active: z.boolean({
     required_error: 'active is required',
     invalid_type_error: 'active must be a boolean',
   }),
+})
+
+PlannerSchema.refine(data => data.startDate < data.finishDate, {
+  message: 'start date must be before finish date',
+}).refine(data => data.finishDate > data.finishDate, {
+  message: 'finish date must be after start date',
 })
 export const PartialPlannerSchema = PlannerSchema.partial()
